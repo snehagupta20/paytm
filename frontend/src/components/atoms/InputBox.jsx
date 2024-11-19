2c86da401eb16c904f56c92dcc84f3fcbc1292cd
sneha gupta <gsneha20012004@gmail.com>
2024-11-19 23:28:26 +0530

    implemented search functionality

--- frontend/src/components/atoms/InputBox.jsx
+++ frontend/src/components/atoms/InputBox.jsx
@@ -1,7 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
+import JSON_DATA from '../../../public/contacts.json';
 
 export function SearchBox(){
+    
+
     return(
-        <input type="text" placeholder="Search Users..." className="border-2 border-solid border-slate-300 p-[0.5rem] mt-4 mr-4 w-webkit-fill rounded-lg"></input>
+        <input type="text" value={searchItem} onChange={handleInputChange} placeholder="Search Users..." className="border-2 border-solid border-slate-300 p-[0.5rem] mt-4 mr-4 w-webkit-fill rounded-lg"></input>
     )
 };
--- frontend/src/components/molecules/Users.jsx
+++ frontend/src/components/molecules/Users.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { SubHeading } from "../atoms/SubHeading";
 import { SearchBox } from "../atoms/InputBox";
 import { UserDetail } from "../atoms/UserDetail";
@@ -6,14 +6,33 @@ import JSON_DATA from '../../../public/contacts.json';
 
 export function Users(){
 
+    const [searchItem, setSearchItem] = useState('');
+    const [filteredUsers, setFilteredUsers] = useState(JSON_DATA.contacts);
+
+    function handleInputChange(event){
+        const searchTerm = event.target.value;
+
+        setSearchItem(searchTerm);
+
+
+        const filteredItems = JSON_DATA.contacts.filter(function(key){
+            return key.name.toLowerCase().includes(searchTerm.toLowerCase());
+        });
+
+        setFilteredUsers(filteredItems);
+    }
+
     return(
         <div className=" px-6">
             <SubHeading>Users</SubHeading>
-            <SearchBox></SearchBox>
+
+            <input type="text" value={searchItem} onChange={handleInputChange} placeholder="Search Users..." className="border-2 border-solid border-slate-300 p-[0.5rem] mt-4 mr-4 w-webkit-fill rounded-lg"></input>
+
             <div className="h-4"></div>
             {
-                JSON_DATA.contacts.map(function(key){
-                    return <UserDetail name={key.name} ></UserDetail>
+
+                filteredUsers.map(function(user){
+                    return <UserDetail name={user.name} ></UserDetail>
                 })
             }
             
